d6ce24f7bfd038e027f29db3c19c8407a66043fe
JOKER_SON <alreadysons@gmail.com>
2024-12-19 20:03:18 +0900

    ♻️ setting 불변객체 쓰레드 세이프방식으로 리팩토링

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,20 @@
+from functools import lru_cache
 from pydantic_settings import BaseSettings
 import os
+from typing import Final
 
-class Setting(BaseSettings) : 
-    RECIPE_DB_API_KEY : str
-    PINECONE_API_KEY : str
-    PINECONE_HOST_URL : str
-    OPENAI_API_KEY : str
-    class Config :
-        env_file = os.getenv("ENV_FILE",".env")
+class Setting(BaseSettings):
+    RECIPE_DB_API_KEY: Final[str]
+    PINECONE_API_KEY: Final[str]
+    PINECONE_HOST_URL: Final[str]
+    OPENAI_API_KEY: Final[str]
+    
+    class Config:
+        env_file = os.getenv("ENV_FILE", ".env")
+        frozen = True  # 불변 객체로 만들기
 
-setting = Setting()
+@lru_cache()
+def get_settings() -> Setting:
+    return Setting()
+
+setting = get_settings()
