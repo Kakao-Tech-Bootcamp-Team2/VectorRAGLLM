224d7426d4533953edaae0a0067a95b7ca9a48f0
JOKER_SON <alreadysons@gmail.com>
2024-12-20 15:30:24 +0900

    ♻️ 데이터 임베딩 기능 , 환경변수, logger,exception 리팩토링

--- app/service/preprocess/data_embedding.py
+++ app/service/preprocess/data_embedding.py
@@ -1,8 +1,12 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from threading import Lock
 from functools import lru_cache
+from app.core import setting
+from app.core.logger import setup_logger
+from app.core.exception import EmbeddingException
 import gc
-from typing import Final
+
+logger = setup_logger(__name__)
 
 class EmbeddingService:
     _lock = Lock()
@@ -12,7 +16,7 @@ class EmbeddingService:
         with cls._lock:
             if cls._instance is None:
                 cls._instance = super().__new__(cls)
-                cls._instance.model_name = "intfloat/multilingual-e5-large-instruct"
+                cls._instance.model_name = setting.MODEL_NAME
                 cls._instance._embedding_model = None
                 cls._instance._model_lock = Lock()
             return cls._instance
@@ -23,11 +27,15 @@ class EmbeddingService:
     @lru_cache(maxsize=1)
     def _load_model(self):
         with self._model_lock:
-            if self._embedding_model is None:
-                self._embedding_model = HuggingFaceEmbeddings(model_name=self.model_name)
+            try:
+                if self._embedding_model is None:
+                    self._embedding_model = HuggingFaceEmbeddings(model_name=self.model_name)
+            except Exception as e:
+                raise EmbeddingException(f"모델 로드 중 오류 발생: {e}")
 
     def _unload_model(self):
         if self._embedding_model is not None:
+            logger.info('model unload')
             del self._embedding_model
             self._embedding_model = None
             gc.collect()  # 메모리 정리
@@ -35,15 +43,19 @@ class EmbeddingService:
     def embed_text(self, texts):
         try:
             self._load_model()
-            result = self._embedding_model.embed_documents(texts)
-            return result
+            logger.info('model embed')
+            return self._embedding_model.embed_documents(texts)
+        except Exception as e:
+            raise EmbeddingException(f"텍스트 임베딩 중 오류 발생: {e}")
         finally:
             self._unload_model()
 
     def embed_query(self, query):
         try:
             self._load_model()
-            result = self._embedding_model.embed_query(query)
-            return result
+            logger.info('model embed')
+            return self._embedding_model.embed_query(query)
+        except Exception as e: 
+            raise EmbeddingException(f"쿼리 임베딩 중 오류 발생: {e}")
         finally:
             self._unload_model()
