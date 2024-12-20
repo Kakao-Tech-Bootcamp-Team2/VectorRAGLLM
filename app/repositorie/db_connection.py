e0f974d2f3d81d0633f8be033eb374fd01642876
JOKER_SON <alreadysons@gmail.com>
2024-12-20 15:29:12 +0900

    ♻️ VectorDB연결 기능 logger,exception 리팩토링

--- app/repositorie/db_connection.py
+++ app/repositorie/db_connection.py
@@ -1,9 +1,14 @@
 from pinecone import Pinecone, ServerlessSpec
 from app.core import setting
-import gc
-from typing import List, Dict, Final
-import re
+from typing import List, Dict
 from threading import Lock
+from app.core.exception import DatabaseException
+from app.core.logger import setup_logger
+from app.core import setting
+import re
+import gc
+
+logger = setup_logger(__name__)
 
 class DatabaseConnection:
     _instance = None
@@ -13,38 +18,36 @@ class DatabaseConnection:
         with cls._lock:
             if cls._instance is None:
                 cls._instance = super().__new__(cls)
-                cls._instance.index_name = "recipes"
-                cls._instance.dimension = 1024
+                cls._instance.index_name = setting.VECTOR_DB_NAME
+                cls._instance.dimension = setting.VECTOR_DIMENSION
                 cls._instance._connection_lock = Lock()
                 cls._instance._index = None
                 cls._instance._pc = None
             return cls._instance
 
-    def __init__(self):
-        pass
-
     def _load_connection(self):
         with self._connection_lock:
-            if self._pc is None:
-                self._pc = Pinecone(api_key=setting.PINECONE_API_KEY)
+            try :
+                if self._pc is None:
+                    logger.info("Pinecone Connection Loading...")
+                    self._pc = Pinecone(api_key=setting.PINECONE_API_KEY)
                 
-                # 인덱스 존재 여부 확인
-                if self.index_name not in self._pc.list_indexes().names():
-                    self._pc.create_index(
-                        name=self.index_name,
-                        dimension=self.dimension,
-                        metric='cosine',
-                        spec=ServerlessSpec(
-                            cloud='aws',
-                            region='us-east-1'
-                        )
+                    # 인덱스 존재 여부 확인
+                    if self.index_name not in self._pc.list_indexes().names():
+                        self._pc.create_index(
+                            name=self.index_name,
+                            dimension=self.dimension,
+                            metric='cosine',
+                            spec=ServerlessSpec(cloud='aws',region='us-east-1')
                     )
-                    print(f"인덱스 '{self.index_name}'가 생성되었습니다.")
+                    logger.info(f"index '{self.index_name}'create.")
                 else:
-                    print(f"인덱스 '{self.index_name}'는 이미 존재합니다. 기존 인덱스를 사용합니다.")
-
+                    logger.info("Pinecone '{self.index_name}'Connection Loaded.")
                 # 인덱스 불러오기
                 self._index = self._pc.Index(self.index_name)
+            except Exception as e: 
+                logger.error(f"Connection Error: {e}")
+                raise DatabaseException("Pinecone Connection Error")
 
     def _unload_connection(self):
         try:
@@ -60,48 +63,27 @@ class DatabaseConnection:
                 
             # 강제로 가비지 컬렉션 실행
             gc.collect()
-            print("Pinecone 연결이 정리되었습니다.")
+            logger.info("Connection Unloaded.")
         except Exception as e:
-            print(f"연결 정리 중 오류 발생: {e}")
-
-    async def upsert_recipe(self, recipe_id, embedding, metadata):
-        try:
-            self._load_connection()
-            # ID로 기존 데이터 검색
-            existing_data = self._index.fetch(ids=[str(recipe_id)])
-            
-            if existing_data.vectors:
-                # 기존 데이터가 있으면 업데이트
-                print(f"레시피 {recipe_id} 업데이트 중...")
-                self._index.update(
-                    id=str(recipe_id),
-                    values=embedding,
-                    metadata=metadata
-                )
-                return {"status": "updated"}
-            else:
-                # 새로운 데이터 삽입
-                print(f"새로운 레시피 {recipe_id} 추가 중...")
-                self._index.upsert(vectors=[{
-                    'id': str(recipe_id),
-                    'values': embedding,
-                    'metadata': metadata
-                }])
-                return {"status": "inserted"}
-        finally:
-            self._unload_connection()
+            logger.error(f"Connection Unload Error: {e}")
+            raise DatabaseException("Pinecone Connection Unload Error")
 
     def query(self, vector, top_k, include_metadata=True):
         try:
             self._load_connection()
-            return self._index.query(
+            result = self._index.query(
                 vector=vector,
                 top_k=top_k,
                 include_metadata=include_metadata
             )
+            return result
+        except Exception as e: 
+            logger.error(f"Query Error: {e}")
+            raise DatabaseException("Pinecone Query Error")
         finally:
             self._unload_connection()
 
+    @staticmethod
     # 재료 이름에서 양과 단위를 제거하는 함수
     def strip_quantities(self, ingredients: List[str]) -> List[str]:
         cleaned_ingredients = []
@@ -122,9 +104,11 @@ class DatabaseConnection:
                 cleaned_ingredients.append(cleaned)
         return cleaned_ingredients
     
+    @staticmethod
     def extract_ingredient_name(self, ingredient:str) -> str:
         # 괄호와 슬래시, 물결표 등 불필요한 문자 제거
         return re.match(r'([^(/~]+)', ingredient).group(1).strip()
     
+    @staticmethod
     def process_ingredients(self, recipe_metadata: Dict) -> List[str]:
         return [self.extract_ingredient_name(ing) for ing in recipe_metadata['ingredients']]
