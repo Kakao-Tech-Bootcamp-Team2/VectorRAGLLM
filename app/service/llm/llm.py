bc6e0dda812b13043061a0bad79fe6f321b57682
JOKER_SON <alreadysons@gmail.com>
2024-12-20 15:29:59 +0900

    ♻️ LLM기능 logger,exception 리팩토링

--- app/service/llm/llm.py
+++ app/service/llm/llm.py
@@ -1,9 +1,12 @@
 from typing import List, Optional, Dict
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
 from app.core import setting
-from .models import RecipeResponse
-from .prompts import prompt
+from app.core.exception import AppException
+from app.core.logger import setup_logger
+from app.service.llm.models import RecipeResponse
+from app.service.llm.prompts import prompt
+
+logger = setup_logger(__name__)
 
 class RecipeGenerator:    
     def __init__(
@@ -12,16 +15,20 @@ class RecipeGenerator:
         temperature: float = 0.3,
         max_tokens: int = 5000,
     ):
-        if not (api_key := setting.OPENAI_API_KEY):
-            raise ValueError("OPENAI_API_KEY 환경 변수가 설정되지 않았습니다.")
-            
-        self.llm = ChatOpenAI(
-            model=model,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            api_key=api_key
-        ).with_structured_output(RecipeResponse)
-
+        try :
+            if not (api_key := setting.OPENAI_API_KEY):
+                raise ValueError("OPENAI_API_KEY 환경 변수가 설정되지 않았습니다.")
+            self.llm = ChatOpenAI(
+                model=model,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                api_key=api_key
+            ).with_structured_output(RecipeResponse)
+            logger.info("LLM Generate Initialized.")
+        except Exception as e:
+            logger.error(f"LLM generate Error: {e}")
+            raise AppException("LLM Generate Error")
+        
     def format_ingredients(self, ingredients: List[Dict[str, str]]) -> str:
         return "\n".join(
             f"{item['ingredients']}: {item['quantities']}" 
@@ -34,19 +41,24 @@ class RecipeGenerator:
     ) -> Optional[RecipeResponse]:
         #레시피 추천 응답을 생성하는 함수
         try:
+            logger.info("Recipe Generation Start.")
             formatted_messages = prompt.format_messages(
                 ingredients=self.format_ingredients(user_ingredients),
                 documents=search_response
             )
             return self.llm.invoke(formatted_messages)
         except Exception as e:
-            print(f"레시피 생성 중 오류 발생: {e}")
-            return None
+            logger.error(f"Recipe Generation Error: {e}")
+            raise AppException("Recipe Generation Error")
 
 # 전역 인스턴스 생성
 recipe_generator = RecipeGenerator()
 
 def generate_response(user_ingredients: List[Dict[str, str]], search_response: str) -> Optional[Dict]:
     #레시피 추천 응답을 생성하는 함수
-    response = recipe_generator.generate_recipes(user_ingredients, search_response)
-    return response.model_dump(by_alias=True) if response else None
+    try :
+        response = recipe_generator.generate_recipes(user_ingredients, search_response)
+        return response.model_dump(by_alias=True) if response else None
+    except Exception as e:
+        logger.error(f"Generate Response Error: {e}")
+        raise AppException("Generate Response Error")
